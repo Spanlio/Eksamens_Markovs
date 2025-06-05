df9b410c9e2f4c76976e85afcf5a0d889439eb21
Spanlio <d.e.markovs@gmail.com>
2025-06-05 16:42:21 +0300

    Pirmie jautajumi

--- Tests.java
+++ Tests.java
@@ -1,7 +1,22 @@
 public class Tests {
     public static void main(String[] args) {
         
-        
+        String [] jautajumi = {"Kas ir priekšnosacījuma cikls Java valodā?\nA) for\nB) do-while\nC) while\nD) switch",
+        // 2. jautajums
+        "Ko izvadīs šis kods?\n\nint i = 0;\nwhile (i < 3){\nSystem.out.println(i);\ni++;\n}",
+        // 3. jautājums
+        "Kāds ir galvenais nosacījums while cikla darbībai?",
+        // 4. jautājums
+        "Ko dara šis cikls?\n\nint i = 5;\nwhile (i > 0) {\nSystem.out.println(i);\ni--;\n}",
+        // 5. jautājums
+        "Kurā brīdī tiek pārbaudīts while cikla nosacījums?",
+        // 6. jautājums
+        "Ko darīs šis kods?\nint i = 0;\nwhile (i < 5) {\nSystem.out.println(\"Java\");\n}\n",
+        // 7. jautājums
+        "Kāds būs rezultāts?\nint i = 10;\nwhile (i < 5) {\nSystem.out.println(i);\ni++;\n;"
+    };
+                                
+            System.out.println(jautajumi[6]);
 
 
     }
